a16a912c2d209882cff84283b5398d59ab07bd1b
praveenkumar11901 <praveenkumar11901@outlook.com>
2024-12-27 00:00:33 +0530

    final code

--- src/components/wallet/WalletConstants.ts
+++ src/components/wallet/WalletConstants.ts
@@ -1,3 +1,3 @@
 import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
-export const aptosConfig = new AptosConfig({ network: Network.DEVNET });
+export const aptosConfig = new AptosConfig({ network: Network.TESTNET });
 export const aptos = new Aptos(aptosConfig);
